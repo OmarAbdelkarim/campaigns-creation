e0c4f26433ef9d6c3c76905d2ab9b480e96c914e
OmarAbdelkarim <64617239+OmarAbdelkarim@users.noreply.github.com>
2025-07-10 22:18:22 +0300

    Update Advanced Concurrency Settings tooltip

--- src/components/NewCampaignModal.tsx
+++ src/components/NewCampaignModal.tsx
@@ -732,9 +732,10 @@ export const NewCampaignModal: React.FC<NewCampaignModalProps> = ({
                             <Phone className="w-4 h-4 mr-1" />
                             Outbound Caller ID
                             <div className="relative ml-2 group">
-                              <Info className="w-4 h-4 text-gray-400 cursor-help" />
+                            {/* Tooltip for disabled state */}
+                            {!formData.ivr && (
                               <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-3 py-2 bg-gray-900 text-white text-xs rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none whitespace-nowrap z-10">
-                                This number will appear as the caller ID to recipients
+                                IVR selection required: Please select an IVR in the configuration above to enable concurrency auto-scaling options.
                                 <div className="absolute top-full left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-4 border-r-4 border-t-4 border-transparent border-t-gray-900"></div>
                               </div>
                             </div>
