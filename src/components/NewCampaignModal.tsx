d05e0775ff49887184aea7961110ca057482fb84
OmarAbdelkarim <64617239+OmarAbdelkarim@users.noreply.github.com>
2025-07-13 13:17:32 +0300

    Updated NewCampaignModal.tsx

--- src/components/NewCampaignModal.tsx
+++ src/components/NewCampaignModal.tsx
@@ -1,9 +1,12 @@
 Here's the fixed version with all missing closing brackets added:
 
 ```typescript
-                              </div>
+                              <div className="absolute left-0 top-2 w-0 h-0 border-t-4 border-b-4 border-r-4 border-transparent border-r-gray-800 -ml-1"></div>
                             </div>
                           </div>
+                        </div>
+                      </label>
+                      <div className="relative group-dropdown-container">
                         </div>
                       </div>
                     </div>
